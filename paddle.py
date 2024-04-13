075b4f3f1cd2a5491ed58e6b57d36dd0363477d9
AdeshGhadage <110734052+AdeshGhadage@users.noreply.github.com>
2024-04-13 15:13:40 +0530

    Add files via upload

---
+++ paddle.py
@@ -0,0 +1,31 @@
+from turtle import Turtle
+
+MOVE_DISTANCE = 50
+MOVABLE_DIS_UPER = 200
+MOVABLE_DIS_DOWN = -200
+
+
+class Paddle(Turtle):
+    def __init__(self, position):
+        super().__init__()
+
+        self.shape("square")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.color("white")
+        self.penup()
+        self.goto(position)
+
+
+    def up(self):
+        if self.ycor() < MOVABLE_DIS_UPER:
+            new_y = self.ycor() + MOVE_DISTANCE
+            self.goto(self.xcor(), new_y)
+
+    def down(self):
+        if self.ycor() > MOVABLE_DIS_DOWN:
+            new_y = self.ycor() - MOVE_DISTANCE
+            self.goto(self.xcor(), new_y)
+
+    def reset_position(self):
+        self.goto(self.xcor(), 0)
+        
